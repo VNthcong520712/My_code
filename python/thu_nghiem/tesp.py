918e6bd45a6db69b273ffa44928e2144e6e110db
VNthcong520712 <thanhcongj26@gmail.com>
2023-05-04 16:24:58 +0700

    local

--- python/thu_nghiem/tesp.py
+++ python/thu_nghiem/tesp.py
@@ -1,6 +1,22 @@
-n = int(input())
-t = 0
-for i in range(n):
-    t += i
+import phonenumbers, opencage, folium
+# from myphone import number
 
-print(t)
+from phonenumbers import geocoder, carrier
+from opencage.geocoder import OpenCageGeocode
+
+pep = phonenumbers.parse("+919775567890")
+local = geocoder.description_for_number(pep, "vi")
+print(carrier.name_for_number(pep, 'vi'), local)
+ser = carrier.name_for_number(pep, "vi")
+
+key = "efa3975761ab4ab68cbbff8043cdc406"
+geo = OpenCageGeocode(key)
+que = str(local)
+result = geo.geocode(que)
+lat = result[0]["geometry"]["lat"]
+lng = result[0]['geometry']['lng']
+print(lat, lng)
+
+map = folium.Map(location=[lat, lng], zoom_control=9)
+folium.Marker([lat, lng], popup=local).add_to(map)
+map.save("mylocation.html")
