0d666a03a1887cbbbbd3dc358ec636176d55a1e5
VNthcong520712 <thanhcongj26@gmail.com>
2023-05-25 18:53:11 +0700

    gptb1

--- C++/luyencode.net/gptb1/gptb1.cpp
+++ C++/luyencode.net/gptb1/gptb1.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int main(){
-	int a, b, c, d, e, f;
+	long long a, b, c, d, e, f;
 	cin >> a >> b >> c >> d >> e >> f;
 	long long tx, ty, tz;
 	double x, y;
+
 	tx = (long)-f*b+(long)e*c;
 	ty = (long)-c*d+(long)f*a;
 	tz = (long)a*e-(long)d*b;
-
-	cout << "tx: " << tx << "  ty: " << ty << "  tz: " << tz << endl;
 
 	if(tz == 0){
 		if(tx == 0 && ty == 0) cout << "VOSONGHIEM";
@@ -19,6 +18,10 @@ int main(){
 	} else {
 		x = (double)tx/tz;
 		y = (double)ty/tz;
+
+		if(x == 0) x = double(0);
+		if(y == 0) y = double(0);
+
 		cout << fixed << setprecision(2) << x << " " << y;
 	}
 }
