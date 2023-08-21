3a966f58f4a1cf11a101ea0059e2f36cf7b19a3e
Thanh Cong <thanhcongj26@gmail.com>
2023-08-21 18:29:07 +0700

    TC code

--- C++/thu_nghiem/test.cpp
+++ C++/thu_nghiem/test.cpp
@@ -1,6 +1,29 @@
+<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
 int main(){
 	
 }
+=======
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int n, max;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        if(i == 0) {
+            max = a[0];
+            continue;
+        }else{
+            if(a[i] > max) max = a[i];
+        } 
+    }
+    cout << max;
+    return 0;
+}
+>>>>>>> a855551 (TC code)
