8810f0ce9e3e0a0007eaad2883af00d4548f5f72
TCGarlic <94214319+VNthcong520712@users.noreply.github.com>
2023-10-21 17:22:55 +0700

    ch

--- C++/thu_nghiem/test.cpp
+++ C++/thu_nghiem/test.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-using namespace std;
+
+int aa(int p){
+	static int d = 15;
+	std::cout << d << " " << p<<"--";
+	d+= p;
+	std::cout << d<<";";
+	return d;
+}
 
 int main(){
-    int ad[32] = {0};
-    for(int i = 0; i < 32; i++ ){
-        cout << ad[i] << endl;
-    }
+	int a = 4;
+	std::cout <<aa(a-1+ aa(a + aa(a-3)));
 }
--- C/test.c
+++ C/test.c
@@ -1,29 +1,9 @@
-#include <stdio.h>
-
-double nhiphan(unsigned long long a){
-    if ( a == 0){
-        return 0;
-    }
-    int d = a%2;
-    long long n = (a-d)/2;
-    return (nhiphan(n)*10 + d);
+#include <iostream.h>
+int d = 1;
+int func(int p){
+    cout << "Ấ";
 }
 
-int main(){
-	long T;
-    scanf("%ld", &T);
-    if ((1 <= T) && (T <= 100000)){
-        unsigned long long n[T];
-        double kq[T];
-        for (int i = 0; i < T; i++){
-            scanf("%lld", &n[i]);
-            if ((n[i] < 1) || (n[i] > 1000000000000000000)){
-                break;
-            }
-            kq[i] = nhiphan(n[i]);
-        }
-        for (int i = 0; i < T; i ++){
-            printf("%.0lf\n", kq[i]);
-        }
-    }
+void main(){
+    func(9);
 }
